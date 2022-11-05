a6eee15d257f846cc5bceed281babd89b7f7d6e7
saratedd <sara.tedesko91@gmail.com>
2022-11-05 17:13:50 +0100

    vul sql done

--- routes/novul.js
+++ routes/novul.js
@@ -1,8 +1,47 @@
 const express = require('express')
 const router = express.Router()
+var format = require('pg-format');
+const pool = require('../public/js/db')
+const dbSetup = require('../public/js/setup')
+
+router.use(express.json())
+
+const attrs = ['user_id', 'user_name', 'user_password']
 
 router.get('/', (req, res) => {
-    res.render('novul')
+
+    pool.query(dbSetup, (err, resu, f) => {
+    })
+
+    res.render('vul', {
+        auth: false,
+        text : 'joj',
+        users: ''
+    })
+})
+// ' or 1=1--
+router.post('/', (req, res) => {
+    var user = req.body.user
+    var pass = req.body.pass
+    var auth = false
+    if (user && pass) auth = true
+    var sql = `select * from users where user_name like '${user}' and user_password like '${pass}'`
+
+    pool.query(sql, (err, result, f) => {
+        if (err) {
+            console.log(err)
+            throw err
+        } else {
+            console.log(result);
+            res.render('vul', {
+                auth: auth,
+                users: result.rows,
+                attrs: attrs,
+                user: user,
+                pass: pass
+            })
+        }
+    })
 })
 
 module.exports = router
--- routes/vul.js
+++ routes/vul.js
@@ -2,28 +2,44 @@ const express = require('express')
 const router = express.Router()
 const pool = require('../public/js/db')
 const dbSetup = require('../public/js/setup')
+router.use(express.json())
 
+const attrs = ['user_id', 'user_name', 'user_password']
 
 router.get('/', (req, res) => {
 
-    pool.query(dbSetup, (err, res, f) => {
-        // console.log(res);
+    pool.query(dbSetup, (err, resu, f) => {
     })
 
     res.render('vul', {
-        text : 'joj'
+        auth: false,
+        text : 'joj',
+        users: ''
     })
 })
-
+// ' or 1=1--
 router.post('/', (req, res) => {
     var user = req.body.user
     var pass = req.body.pass
-    console.log(body);
-    res.render('vul', {
-        text: body
+    var auth = false
+    if (user && pass) auth = true
+    var sql = `select * from users where user_name like '${user}' and user_password like '${pass}'`
+
+    pool.query(sql, (err, result, f) => {
+        if (err) {
+            console.log(err)
+            throw err
+        } else {
+            console.log(result);
+            res.render('vul', {
+                auth: auth,
+                users: result.rows,
+                attrs: attrs,
+                user: user,
+                pass: pass
+            })
+        }
     })
 })
-
-
 
 module.exports = router
