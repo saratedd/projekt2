2fa473f03d0249d0924e724a4b4ba3f4f159c4ea
saratedd <sara.tedesko91@gmail.com>
2022-11-05 09:54:48 +0100

    setup

--- server.js
+++ server.js
@@ -2,14 +2,26 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res/*.send('homepage')*/.status(200)
-    res.render("index", {
-        text: "welcome text"
-    })
+app.use(express.static('public'))
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//     res/*.send('homepage')*/.status(200)
+//     res.render("index", {
+//         text: "welcome text"
+//     })
+// })
+
+app.use('/:smth', (req, res) => {
+    res.send(`The page with this url (localhost:3000/${req.params.smth}) does not exist. Go somewhere else.`).status(404)
 })
+
+const vulRouter = require('./routes/vul')
+const novulRouter = require('./routes/novul')
+app.use('/vul', vulRouter)
+app.use('/novul', vulRouter)
 
 
 app.listen(3000)
